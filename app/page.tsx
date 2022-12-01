9595e00ca74ea023da23fa9c7862cfeba3d0ebc1
Tat Leung <tat.leung@piercewashington.com>
2022-11-30 20:45:27 -0800

    a bit of cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { Button, Grid, Stack } from "@mui/material";
+import { Button, Grid, Stack, Typography } from "@mui/material";
 export default function Home() {
   return (
     <Grid container height="100vh" alignItems="center" justifyContent="center" direction="column">
-      <h1>Using Material UI with Next.js 13</h1>
+      <Typography variant="h4">Application startup template with Next.js 13, MUI, and Tailwind CSS</Typography>
       <Stack direction="row" columnGap={1}>
         <Button variant="text" className="text-red-800">Text</Button>
         <Button variant="contained">Contained</Button>
